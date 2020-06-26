58b3b1fdcab4c2e762f230703f84577f61bbf4c1
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2020-06-26 12:20:01 +0200

    Add initial Package.swift and Sources

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.2
+
+import PackageDescription
+
+let package = Package(
+    name: "xcdoctor",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.2.0"),
+    ],
+    targets: [
+        .target(
+            name: "xcdoctor",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ]),
+    ]
+)
