41d12f2b6b9cd78792fd4d0bffef745d03e523fd
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2020-06-26 21:40:39 +0200

    Refactor nonExistentFiles defect

--- Sources/XCDoctor/Diagnosis.swift
+++ Sources/XCDoctor/Diagnosis.swift
@@ -18,23 +18,33 @@ public struct Diagnosis {
     public let cases: [String]?
 }
 
+func nonExistentFilePaths(in project: XcodeProject) -> [String] {
+    project.fileUrls.filter { url -> Bool in
+        // include this url if file does not exist
+        !FileManager.default.fileExists(atPath: url.path)
+    }.map { url -> String in
+        // transform url to readable path
+        url.standardized.relativePath
+    }
+}
+
 public func examine(project: XcodeProject, for defects: [Defect]) -> [Diagnosis] {
     var diagnoses: [Diagnosis] = []
     // TODO: separate each examination into smaller parts; a func per defect?
     for defect in defects {
         switch defect {
         case .nonExistentFiles:
-            let nonExistentFileUrls = project.fileUrls.filter { url -> Bool in
-                !FileManager.default.fileExists(atPath: url.path)
-            }
-            if !nonExistentFileUrls.isEmpty {
-                let paths = nonExistentFileUrls.map { (url) -> String in
-                    url.standardized.relativePath
-                }
+            let files = nonExistentFilePaths(in: project)
+            if !files.isEmpty {
                 diagnoses.append(Diagnosis(
-                    conclusion: "non-existent files are referenced in project",
-                    help: "File references to non-existent files should be removed from the project.",
-                    cases: paths
+                    conclusion: "non-existent file(s) referenced in project",
+                    // TODO: this text should be wrapped at X columns; can do manually, but ...
+                    help: """
+                    These files might have been moved or removed from the filesystem.
+                    In either case, each reference should be removed from the project;
+                    if a file has been moved, add back the file from its new location.
+                    """,
+                    cases: files
                 ))
             }
         }
