6757eb5b1e666632e411fb90925708168e1bd4f4
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2020-06-28 19:15:33 +0200

    Add tests

--- Tests/XcodeProjectTests.swift
+++ Tests/XcodeProjectTests.swift
@@ -11,28 +11,45 @@ import Foundation
 import XCTest
 
 class XcodeProjectTests: XCTestCase {
+    func projectUrl(for defect: Defect) -> URL {
+        // note that this assumes `$ swift test` from the root of the project;
+        // it does not work when run from Xcode
+        return URL(fileURLWithPath: "Tests/Subjects/")
+            .appendingPathComponent(
+                "\(defect)/xcdoctor.xcodeproj/project.pbxproj")
+    }
+
     func testProjectNotFound() {
         XCTAssertNil(XcodeProject(from:
+            // assuming this path does not exist!
             URL(fileURLWithPath: "~/Some/Project.xcodeproj")))
     }
 
     func testProjectFound() {
-        // note that this path assumes `$ swift test` from the root of the project;
-        // it does not work when run from Xcode
-        XCTAssertNotNil(XcodeProject(from:
-            URL(fileURLWithPath: "Tests/Subjects/missing-file.xcodeproj/project.pbxproj")))
+        let project = XcodeProject(
+            from: projectUrl(for: .nonExistentFiles))!
+        XCTAssertNotNil(project)
     }
 
     func testFileUrls() {
-        let project = XcodeProject(from:
-            URL(fileURLWithPath: "Tests/Subjects/missing-file.xcodeproj/project.pbxproj"))!
+        let project = XcodeProject(
+            from: projectUrl(for: .nonExistentFiles))!
         XCTAssert(project.files.count == 1)
     }
 
     func testMissingFile() {
-        let project = XcodeProject(from:
-            URL(fileURLWithPath: "Tests/Subjects/missing-file.xcodeproj/project.pbxproj"))!
+        let project = XcodeProject(
+            from: projectUrl(for: .nonExistentFiles))!
         let diagnosis = examine(project: project, for: .nonExistentFiles)
+        XCTAssertNotNil(diagnosis)
+        XCTAssertNotNil(diagnosis!.cases)
+        XCTAssert(diagnosis!.cases!.count == 1)
+    }
+
+    func testCorruptPlist() {
+        let project = XcodeProject(
+            from: projectUrl(for: .corruptPropertyLists))!
+        let diagnosis = examine(project: project, for: .corruptPropertyLists)
         XCTAssertNotNil(diagnosis)
         XCTAssertNotNil(diagnosis!.cases)
         XCTAssert(diagnosis!.cases!.count == 1)
