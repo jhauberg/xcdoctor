d4fcccb647e18c4ebeb65c185f882aeaf3aa30c2
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2020-06-28 20:34:57 +0200

    Bump version

--- Sources/CLI/main.swift
+++ Sources/CLI/main.swift
@@ -39,7 +39,7 @@ import ArgumentParser
 struct Doctor: ParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "xcdoctor",
-        version: "0.1.0"
+        version: "0.2.0"
     )
 
     @Argument(help:
