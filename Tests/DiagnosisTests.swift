72813e3678fd85cd7edda12b3b25b6c5fd02c47b
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2020-07-14 20:44:32 +0200

    Refactor diagnosis tests into separate file

--- Tests/DiagnosisTests.swift
+++ Tests/DiagnosisTests.swift
@@ -1,8 +1,8 @@
 //
-//  XcodeProjectTests.swift
-//  xcdoctor
+//  DiagnosisTests.swift
+//  XCDoctorTests
 //
-//  Created by Jacob Hauberg Hansen on 26/06/2020.
+//  Created by Jacob Hauberg Hansen on 14/07/2020.
 //
 
 @testable import XCDoctor
@@ -10,69 +10,7 @@
 import Foundation
 import XCTest
 
-// note that paths in these tests assumes `$ swift test` from the root of the project;
-// it does _not_ work when run from Xcode; some might still pass, but most will fail
-func projectUrl(for defect: Defect) -> URL {
-    URL(fileURLWithPath: "Tests/Subjects/")
-        .appendingPathComponent(
-            "\(defect)/xcdoctor.xcodeproj"
-        )
-}
-
-class XcodeProjectTests: XCTestCase {
-    func testProjectNotFoundInNonExistentPath() {
-        let result = XcodeProject
-            .openAndEvaluate(from: URL(fileURLWithPath: "~/Some/Project.xcodeproj"))
-        XCTAssertThrowsError(try result.get()) { error in
-            XCTAssertEqual(
-                error as! XcodeProjectError,
-                XcodeProjectError.notFound(amongFilesInDirectory: false)
-            )
-        }
-    }
-
-    func testProjectNotFoundInNonExistentDirectory() {
-        let result = XcodeProject.openAndEvaluate(from: URL(fileURLWithPath: "~/Some/Place/"))
-        XCTAssertThrowsError(try result.get()) { error in
-            XCTAssertEqual(
-                error as! XcodeProjectError,
-                XcodeProjectError.notFound(amongFilesInDirectory: true)
-            )
-        }
-    }
-
-    func testProjectNotFoundInDirectory() {
-        // assumes this directory is kept rid of .xcodeprojs
-        let result = XcodeProject.openAndEvaluate(from: URL(fileURLWithPath: "Tests/Subjects/"))
-        XCTAssertThrowsError(try result.get()) { error in
-            XCTAssertEqual(
-                error as! XcodeProjectError,
-                XcodeProjectError.notFound(amongFilesInDirectory: true)
-            )
-        }
-    }
-
-    func testProjectFound() {
-        let result = XcodeProject.openAndEvaluate(from: projectUrl(for: .nonExistentFiles))
-        XCTAssertNoThrow(try result.get())
-    }
-
-    func testFileReferenceResolution() {
-        let condition: Defect = .nonExistentFiles
-        let result = XcodeProject.openAndEvaluate(from: projectUrl(for: condition))
-        guard let project = try? result.get() else {
-            XCTFail(); return
-        }
-        XCTAssert(project.files.count == 1)
-        XCTAssertEqual(
-            project.files.first!.url,
-            projectUrl(for: condition)
-                .deletingLastPathComponent()
-                .appendingPathComponent("xcdoctor")
-                .appendingPathComponent("main.swift")
-        )
-    }
-
+class DiagnosisTests: XCTestCase {
     func testMissingFile() {
         let result = XcodeProject.openAndEvaluate(from: projectUrl(for: .nonExistentFiles))
         guard let project = try? result.get() else {
